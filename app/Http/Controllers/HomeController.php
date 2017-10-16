c366495e53c8d9398df421bfcd67237988166dae
Duilio Palacios <duilio@styde.net>
2017-10-16 17:26:59 +0100

    Move auth middleware to route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,20 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
