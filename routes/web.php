5cd53dc0d6546bfb6dc125426eff71fd0d647d08
Abel Ponce <vnponce8@gmail.com>
2017-10-21 12:42:38 -0500

    Solved lesson 2 exercise

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+Route::view('/change-password', 'auth.passwords.change-password')->name('change_password')->middleware('auth');
