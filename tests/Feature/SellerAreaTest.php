0970195100ac2b0e60c44b34d95df1bd71eb17dd
David Palacios <dvalpx@gmail.com>
2017-10-23 17:05:37 -0400

    Seller and customer tests.

---
+++ tests/Feature/SellerAreaTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SellerAreaTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function sellers_can_visit_the_sellers_area()
+    {
+        $seller = factory(User::class)->create([
+            'seller' => true
+        ]);
+
+        $this->actingAs($seller)
+            ->get(route('sellers'))
+            ->assertStatus(200)
+            ->assertSee('Sellers Area');
+    }
+
+    /** @test */
+    function customers_cannot_visit_the_sellers_area()
+    {
+        $customer = factory(User::class)->create([
+            'customer' => true
+        ]);
+
+        $this->actingAs($customer)
+            ->get(route('sellers'))
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    function non_sellers_cannot_visit_the_sellers_area()
+    {
+        $user = factory(User::class)->create([
+            'seller' => false
+        ]);
+
+        $this->actingAs($user)
+            ->get(route('sellers'))
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    function guests_cannot_visit_the_sellers_area()
+    {
+        $this->get(route('sellers'))
+            ->assertStatus(302)
+            ->assertRedirect('login');
+    }
+}
