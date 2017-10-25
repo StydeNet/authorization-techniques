27c4161c4d87fe1cced260fa5835ca2757e84148
David Palacios <dvalpx@gmail.com>
2017-10-24 23:27:42 -0400

    Add lesson 5 exercises.

---
+++ tests/Feature/CustomerAreaTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CustomerAreaTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function customers_can_visit_the_customers_area()
+    {
+        $customer = factory(User::class)->create([
+            'role' => 'customer'
+        ]);
+
+        $this->actingAs($customer)
+            ->get(route('customers'))
+            ->assertStatus(200)
+            ->assertSee('Customers Area');
+    }
+
+    /** @test */
+    function sellers_cannot_visit_the_customers_area()
+    {
+        $seller = factory(User::class)->create([
+            'role' => 'seller'
+        ]);
+
+        $this->actingAs($seller)
+            ->get(route('customers'))
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    function non_customers_cannot_visit_the_customers_area()
+    {
+        $user = factory(User::class)->create([
+            'role' => false
+        ]);
+
+        $this->actingAs($user)
+            ->get(route('customers'))
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    function guests_cannot_visit_the_customers_area()
+    {
+        $this->get(route('customers'))
+            ->assertStatus(302)
+            ->assertRedirect('login');
+    }
+}
